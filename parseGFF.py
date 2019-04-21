79746855ce5d83c33b3062bf1fd121e5bf0f4c5b
Katy Dupree <kedupree@uark.edu>
2019-04-21 11:56:01 -0500

    parsegff assn 07 with functions

--- parseGFF.py
+++ parseGFF.py
@@ -8,28 +8,9 @@ import argparse
 import csv
 from Bio import SeqIO
 
-
-# open the output file
-with open('watermelon_gff.out', 'w') as tabout:
-
-	# open the data file
-	with open('watermelon_gff', 'r') as gff:
-
-		# create a csv reader object
-		csvreader = csv.reader(gff, delimiter='\n')
-
-		header = next(reader)
-
-		# read in the file, line by line
-		for line in reader:
-
-			# skip blank lines
-			if not line:
-				continue
-
-			else:
-				linewriter = csv.writer(tabout, delimiter='\n', quotechar='"')
-				linewriter.writerow(line)
+# specify the input files
+gff_file_path   = 'watermelon.gff'
+fasta_file_path = 'watermelon.fsa'
 
 def get_args():
 #parser description
@@ -40,7 +21,32 @@ def get_args():
 	parser.add_argument("fsa_file_path", help="the path and name of the fsa file")
 
 #parse the command line arguments
-	args = parser.parse_args()
+	return parser.parse_args()
+
+
+def parse_gff():
+# open the output file
+	with open('watermelon_gff.out', 'w') as tabout:
+
+	# open the data file
+		with open(args.gff_file_path, 'r') as gff:
+
+		# create a csv reader object
+			csvreader = csv.reader(gff, delimiter='\n')
+
+			header = next(reader)
+
+		# read in the file, line by line
+			for line in reader:
+
+			# skip blank lines
+				if not line:
+					continue
+
+				else:
+					linewriter = csv.writer(tabout, delimiter='\n', quotechar='"')
+					linewriter.writerow(line)
+
 
 #create a fuction to calculate gc content
 def get_gc_content(substring, sig_figs = 2):
@@ -50,46 +56,53 @@ def get_gc_content(substring, sig_figs = 2):
 	gc_content = (c_count + g_count) / length
 	return round(gc_content, sig_figs)
 
-#Read in files
-gff = open(args.gff_file_path, 'r')
-fsa = open(args.fsa_file_path).read()       
 
-# specify the input files
-gff_file   = 'watermelon_gff.out'
-fasta_file = 'watermelon.fsa'
+def parse_fasta():
+	# open the FASTA file
+	genome = SeqIO.read('watermelon.fsa', 'fasta')
 
+	# print(genome.seq)
+	print(genome)
 
-# open the FASTA file
-genome = SeqIO.read('watermelon.fsa', 'fasta')
+def get_genome_feat():
+	# open the GFF file
+	gff = open('watermelon_gff.out', 'r')
 
-# print(genome.seq)
-print(genome)
-
-# open the GFF file
-gff = open(gff_file, 'r')
-
-# read GFF file, line by line
-for line in gff:
-	# skip blank lines
-	if not line:
-		continue
-	else:
-	# remove line breaks
-		line = line.rstrip('\n')
+	# read GFF file, line by line
+	for line in gff:
+		# skip blank lines
+		if not line:
+			continue
+		else:
+		# remove line breaks
+			line = line.rstrip('\n')
 	
-	# sequence, source, feature, begin, end, length, strand, phase, attributes = line.split('\t')
-		fields = line.split('\t')
+		# sequence, source, feature, begin, end, length, strand, phase, attributes = line.split('\t')
+			fields = line.split('\t')
 	
-	# get fields[3] and fields[4]
-		start = (int(fields[3])-1)	
-		end = (int(fields[4])-1)
-		name =(str(fields[8])[0:11])
+		# get fields[3] and fields[4]
+			start = (int(fields[3])-1)	
+			end = (int(fields[4])-1)
+			name =(str(fields[8])[0:11])
 	
-	# extract the DNA sequence from the genome for this feature
-		output = genome[start:end]
+		# extract the DNA sequence from the genome for this feature
+			output = genome[start:end]
 	
-	# print the DNA sequence for this feature
-	# calculate the GC content for this feature, and print it to the screen
-		print(name, ': ',get_gc_content(output))
-		gff.close()
+		# print the DNA sequence for this feature
+		# calculate the GC content for this feature, and print it to the screen
+			return get_gc_content(output)
+			gff.close()
+
+def main():
+	genome = parse_fasta() 
+	g_c = get_genome_feat()
+	print_output(g_c)
+
+	# get the arguments before calling main
+	args = get_args()
+
+
+	# execute the program by calling main
+if __name__ == "__main__":
+	main()
 
